0312640fc658c39afcede9c45ee781069aeea745
Robelio Barreto <robeliobasantos@gmail.com>
2023-01-08 22:56:08 -0300

    Modificando os arquivos da pasta Models e o arquivo Program.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace DesafioPOO.Models
 {
-    public abstract class Smartphone
+  public abstract class Smartphone
+  {
+    public string Numero { get; set; }
+    public string Modelo { get => modelo; set => modelo = value; }
+    public string Imei { get => imei; set => imei = value; }
+    public int Memoria { get => memoria; set => memoria = value; }
+
+    private string modelo;
+    private string imei;
+    private int memoria;
+    public Smartphone(string numero, string modelo, string imei, int memoria)
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
-
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
-
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
-
-        public abstract void InstalarAplicativo(string nomeApp);
+      Numero = numero;
+      Modelo = modelo;
+      Imei = imei;
+      Memoria = memoria;
     }
+
+    public void Ligar()
+    {
+      Console.WriteLine("Ligando...");
+    }
+
+    public void ReceberLigacao()
+    {
+      Console.WriteLine("Recebendo ligação...");
+    }
+
+    public abstract void InstalarAplicativo(string nomeApp);
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "071999999999", modelo: "Nokia NK40", imei: "33333333", memoria: 40);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "071888888888", modelo: "Iphone PHN23", imei: "47333333", memoria: 120);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
